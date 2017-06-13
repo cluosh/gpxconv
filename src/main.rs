cfd620bd97d5e53f6c9937e678101666dc70d044
Michael Pucher <michael.pucher.main@gmail.com>
2017-06-13 11:28:30 +0200

    Adding matlab writer

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,163 @@
 extern crate hyper;
 extern crate multipart;
 extern crate regex;
+extern crate flate2;
+extern crate byteorder;
 
 use std::io::Read;
+use std::io::Write;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::fs::File;
+use hyper::Client;
+use multipart::client::lazy::Multipart;
+use regex::Regex;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+use byteorder::{LittleEndian, WriteBytesExt};
 
-fn convert_gpx(filename: &str) -> String {
-    use hyper::Client;
-    use multipart::client::lazy::Multipart;
-    use std::fs::File;
-    use std::io::BufReader;
-
+fn read_gpx(filename: &str) -> String {
+    let mut gpx_data = String::new();
     let gpx_file = File::open(filename)
         .expect("Could not open GPX file for reading");
-    let mut buf_reader = BufReader::new(gpx_file);
-    let mut gpx = String::new();
-    buf_reader.read_to_string(&mut gpx);
+    BufReader::new(gpx_file)
+        .read_to_string(&mut gpx_data)
+        .expect("Could not read GPX file data");
+    gpx_data
+}
 
-    let hyper_client = Client::new();
-    let mut response = Multipart::new()
+fn convert_gpx(filename: &str) -> String {
+    let client = Client::new();
+    let gpx_data = read_gpx(filename);
+    let mut html = String::new();
+    let mut csv = String::new();
+
+    Multipart::new()
         .add_text("convert_format", "text")
         .add_stream("uploaded_file_1",
-                    gpx.as_bytes(),
+                    gpx_data.as_bytes(),
                     Option::Some(filename),
                     None)
+        .add_text("convert_delimiter", "comma")
         .add_text("convert_add_speed", "1")
         .add_text("convert_add_slope", "1")
         .add_text("add_elevation", "SRTM1")
         .add_text("units", "metric")
         .add_text("submitted", "Convert")
-        .add_text("convert_delimiter", "")
-        // .add_text("data", "name,desc,latitude,longitude")
-        // .add_text("force_type", "")
-        // .add_text("remote_data", "")
-        // .add_text("convert_delimiter", "")
-        // .add_text("vmg_point", "")
-        // .add_text("add_elevation", "SRTM1")
-        // .add_text("show_trk", "1")
-        // .add_text("reverse", "0")
-        // .add_text("connect_segments", "0")
-        // .add_text("trk_merge", "0")
-        // .add_text("trk_distance_threshold", "")
-        // .add_text("trk_simplify", "")
-        // .add_text("trk_stats", "0")
-        // .add_text("trk_elevation_threshold", "")
-        // .add_text("tickmark_interval", "")
-        // .add_text("trk_as_wpt", "0")
-        // .add_text("trk_as_wpt_name", "")
-        // .add_text("trk_as_wpt_desc", "")
-        // .add_text("convert_gpx_styles", "")
-        // .add_text("trk_segment_time", "")
-        // .add_text("add_timestamps", "")
-        // .add_text("show_wpt", "3")
-        // .add_text("synthesize_name", "")
-        // .add_text("synthesize_desc", "")
-        // .add_text("reference_point", "")
-        // .add_text("reference_point_name", "")
-        // .add_text("wpt_interpolate", "1")
-        // .add_text("wpt_interpolate_offset", "")
-        // .add_text("convert_repeat_headers", "1")
-        // .add_text("time_offset", "")
-        // .add_text("utm_output", "0")
-        // .add_text("moving_average", "1")
-        // .add_text("frequency_count", "none")
-        // .add_text("special", "")
-        // .add_text("cumulative_distance", "0")
-        // .add_text("tickmark_zero", "1")
-        // .add_text("wifi_mode", "3")
-        // .add_text("forerunner_laps", "1")
-        // .add_text("gps_altitude", "1")
-        // .add_text("trk_preserve_attr", "1")
-        // .add_text("wpt_preserve_attr", "1")
-        // .add_text("convert_routes", "t_aw")
-        // .add_text("padding", "10")
-        // .add_text("wpt_name_filter", "")
-        // .add_text("wpt_desc_filter", "")
-        // .add_text("convert_add_climb", "")
-        // .add_text("convert_add_slope_degrees", "")
-        // .add_text("trk_reorder", "")
-        // .add_text("trk_reorder_merge", "")
-        // .add_text("wpt_polygons", "")
-        .client_request(&hyper_client, "http://www.gpsvisualizer.com/convert?output")
-        .expect("Could not convert GPX data on GPSVisualizer website");
+        .client_request(&client, "http://www.gpsvisualizer.com/convert?output")
+        .expect("Could not convert GPX data on GPSVisualizer website")
+        .read_to_string(&mut html)
+        .expect("Could not read GPSVisualizer response");;
 
-    let mut text = "".to_string();
-    response.read_to_string(&mut text).expect("Could not read GPSVisualizer response");
-
-    let re = regex::Regex::new(r"(/download/convert/[0-9]+\-[0-9]+\-data\.txt)").unwrap();
-    if let Some(cap) = re.captures_iter(&text).next() {
+    let re = Regex::new(r"(/download/convert/[0-9]+\-[0-9]+\-data\.csv)")
+        .expect("Could not bulid regex");
+    if let Some(cap) = re.captures_iter(&html).next() {
         let url = "http://www.gpsvisualizer.com/".to_string() + &cap[0];
-        let mut response = hyper_client.get(&url).send().unwrap();
-
-        let mut text = "".to_string();
-        response.read_to_string(&mut text).expect("Could not read GPSVisualizer response");
-        println!("{}", &text);
+        client
+            .get(&url)
+            .send()
+            .expect("Could not download CSV from GPSVisualizer")
+            .read_to_string(&mut csv)
+            .expect("Could not read downloaded CSV from GPSVisualizer");
     }
 
-    "DONE".to_string()
+    csv
+}
+
+fn parse_csv(csv: &str) -> Vec<(f64,f64)> {
+    let lines: Vec<_> = csv.split("\n").collect();
+    let mut data = Vec::<(f64,f64)>::with_capacity(lines.len());
+
+    for line in lines {
+        let mut values = line.split(",");
+        match values.nth(0) {
+            Some(t) => {
+                if t != "T" {
+                    continue;
+                }
+            }
+            None => continue,
+        }
+
+        let corrected_speed: f64;
+        match values.nth(4) {
+            Some("") => continue,
+            Some(speed) => {
+                corrected_speed = speed
+                    .parse::<f64>()
+                    .expect("Invalid value for speed in CSV");
+            }
+            None => continue,
+        }
+
+        let mut corrected_slope: f64 = 0.0;
+        match values.next() {
+            Some(slope) => {
+                if let Ok(value) = slope.parse::<f64>() {
+                    corrected_slope = value;
+                }
+            }
+            None => continue,
+        }
+
+        data.push((corrected_speed, corrected_slope));
+    }
+    data
+}
+
+fn write_mat(filename: &str, data: &[(f64,f64)]) -> std::io::Result<()> {
+    let mat = File::create(filename)
+        .expect("Could not create MAT file");
+    let mut writer = BufWriter::new(mat);
+
+    // Header with description, version and endian info
+    let header_text = "MATLAB 5.0 MAT-file, Platform: PCWIN64".to_string();
+    writer.write_all(header_text.as_bytes())?;
+    writer.write_all(" ".repeat(116 - header_text.len()).as_bytes())?;
+    writer.write_all(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])?;
+    writer.write_all(&[0x00, 0x01, 0x49, 0x4d])?;
+
+    // miMatrix
+    let mut gz = ZlibEncoder::new(Vec::new(), Compression::Default);
+    gz.write(&[0x0e, 0x00, 0x00, 0x00])?;
+    gz.write_u32::<LittleEndian>(data.len() as u32 * 16 + 48)?;
+
+    // miUINT32, array flags
+    gz.write(&[0x06, 0x00, 0x00, 0x00])?;
+    gz.write(&[0x08, 0x00, 0x00, 0x00])?;
+    gz.write(&[0x06, 0x00, 0x00, 0x00])?;
+    gz.write(&[0x00, 0x00, 0x00, 0x00])?;
+
+    // miINT32
+    gz.write(&[0x05, 0x00, 0x00, 0x00])?;
+    gz.write(&[0x08, 0x00, 0x00, 0x00])?;
+    gz.write_u32::<LittleEndian>(data.len() as u32)?;
+    gz.write(&[0x02, 0x00, 0x00, 0x00])?;
+
+    // miINT8, var name
+    gz.write(&[0x01, 0x00, 0x03, 0x00, 0x74, 0x78, 0x74, 0x00])?;
+
+    // miDOUBLE, actual data
+    gz.write(&[0x09, 0x00, 0x00, 0x00])?;
+    gz.write_u32::<LittleEndian>(data.len() as u32 * 16)?;
+    for &(speed, _) in data {
+        gz.write_f64::<LittleEndian>(speed)?;
+    }
+    for &(_, slope) in data {
+        gz.write_f64::<LittleEndian>(slope)?;
+    }
+
+    // Compressed data output
+    let compressed_data = gz.finish()?;
+    writer.write_all(&[0x0f, 0x00, 0x00, 0x00])?;
+    writer.write_u32::<LittleEndian>(compressed_data.len() as u32)?;
+    writer.write_all(&compressed_data)?;
+    Ok(())
 }
 
 fn main() {
-    println!("{}", convert_gpx("/home/cluosh/work/gps/gps/test.gpx"));
+    let csv = convert_gpx("/home/cluosh/work/gps/gps/test.gpx");
+    let data = parse_csv(&csv);
+    write_mat("/home/cluosh/work/gps/gps/test.mat", &data)
+        .expect("Failed to write MAT file");
 }
